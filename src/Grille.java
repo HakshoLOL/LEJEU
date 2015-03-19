9064d85e57a2ea20e98dfbf021076691693e1c09
HakshoLOL <adri1du54@hotmail.fr>
2015-03-19 15:49:10 +0100

    Revert "Revert "cc""
    This reverts commit 71ffb343962adb7ac7bf3f1b2b5c698d2f704844.

---
+++ src/Grille.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+/**
+ * Classe représentant la grille de jeu contenant les cases
+ * 
+ * @author Adrien
+ *
+ */
+public class Grille {
+
+	/**
+	 * Liste des cases
+	 */
+	private Case[][] lcase;
+	
+	/**
+	 * Contruit une nouvelle grille
+	 * avec 2 cases de 0 ou 4 en valeur
+	 * 
+	 */
+	public Grille() {
+		// a changer par des randoms
+		this.lcase=new Case[4][4];
+		for (int i=0;i<2;i++) {
+			double ale=(Math.random());
+			int val;
+			if (ale <= 0.6) { // 60% 2
+				val=2;
+			} else { // 40% 4
+				val=4;
+			}
+			ajouteCase(new Case(val));
+		}
+	}
+	
+	/**
+	 * retourne dle tableau des cases des cases 
+	 * @return liste des cases
+	 */
+	public Case[][] getLiCases() {
+		return this.lcase;
+	}
+	
+	public void ajouteCase(Case c){
+		ArrayList vide = this.caseVides();
+		int rand = (int)(Math.random()*(vide.size()));
+		int[] indice= (int[]) vide.get(rand);
+		this.lcase[indice[0]][indice[1]] = c;
+	}
+	
+	public ArrayList caseVides() {
+		ArrayList lvide = new ArrayList();
+		for (int i=0;i<this.lcase.length;i++) {
+			for(int j=0;j<this.lcase[i].length;j++) {
+				if (this.lcase[i][j] == null) {
+					int[] tt = {i,j};
+					lvide.add(tt);
+				} 
+			}
+			
+		}
+		return lvide;	
+	}
+	
+	public ArrayList casePleine() {
+		ArrayList lvide = new ArrayList();
+		for (int i=0;i<this.lcase.length;i++) {
+			for(int j=0;j<this.lcase[i].length;j++) {
+				if (this.lcase[i][j] != null) {
+					int[] tt = {i,j};
+					lvide.add(tt);
+				} 
+			}
+			
+		}
+		return lvide;	
+	}
+	
+	public void mvntDroite() {
+		ArrayList pleine = this.casePleine();	
+		for (int i=0;i<pleine.size();i++) {
+			int [] ligne = (int [])pleine.get(i);
+			if ( ligne[i] == i ){
+				System.out.println(ligne[0]+"  "+ligne[1]);
+			}
+		}
+		
+		
+		
+	}
+
+	
+	
+}
+
+
