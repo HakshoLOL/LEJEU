a3c9b5576bb97fbdd9ce7e355513f1a9543c0371
HakshoLOL <adri1du54@hotmail.fr>
2015-03-20 10:04:30 +0100

    les classes
    tada

---
+++ Case.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+
+public class Case {
+	
+	private int val;
+	
+	public Case(int n) {
+		this.val = n;
+	}
+
+	public int getVal() { return this.val; }
+	
+
+
+}
