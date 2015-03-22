ec9ee54eb4257a44cf36abaa9b2cf96bc6c85689
HakshoLOL <adri1du54@hotmail.fr>
2015-03-22 17:33:20 +0100

    mvnt droit quand 1 seul element

--- Case.java
+++ Case.java
@@ -9,5 +9,6 @@ public class Case {
 	}
 
 	public Position getPos() { return this.pos; }
+	public void setPos(Position pp) { this.pos = pp; }
 	public int getVal() { return this.val; }
 }
--- Grille.java
+++ Grille.java
@@ -24,6 +24,8 @@ public class Grille {
 		this.lcase=new Case[this.TAILLE][this.TAILLE];
 		this.ajouteCaseAleat();
 		this.ajouteCaseAleat();
+		this.ajouteCaseAleat();
+		this.ajouteCaseAleat();
 	}
 	
 	public Case[][] getLiCases() {	return this.lcase;	}
@@ -60,10 +62,10 @@ public class Grille {
 	 * @return positions libres
 	 */
 	public ArrayList<Position> caseVides() {
-		ArrayList<Position> liVide = new ArrayList<Position>();
-		for (int i=0;i<this.lcase.length;i++){
-			for (int j=0;j<this.lcase[i].length;j++) {
-				if(this.lcase[i][j]== null ) {
+		ArrayList<Position> liVide = new ArrayList<Position>(); // liste vide
+		for (int i=0;i<this.lcase.length;i++){ // parcourt de "x"
+			for (int j=0;j<this.lcase[i].length;j++) { // pacourt des "y"
+				if(this.lcase[i][j]== null ) { // si null ajou a la liste des cases vides
 					liVide.add(new Position(i,j));
 				}
 			}
@@ -71,6 +73,58 @@ public class Grille {
 		return liVide;	
 	}
 		
+	public ArrayList<Position> casePleines() {
+		ArrayList<Position> liPlein = new ArrayList<Position>(); 
+		for (int i=0;i<this.lcase.length;i++){ // parcourt de "x"
+			for (int j=0;j<this.lcase[i].length;j++) { // pacourt des "y"
+				if(this.lcase[i][j]!= null ) { // si null ajou a la liste des cases pleine
+					liPlein.add(new Position(i,j));
+				}
+			}
+		}
+		return liPlein;	
+	}
+	
+
+	
+	public void mvntDroit() {
+		ArrayList<Position> lplein = this.casePleines();// récupération des cases pleines
+		int temp = -1;
+		for (int i=0;i<lplein.size();i++) { // parcourt des x avec des cases
+	
+			if (temp != lplein.get(i).getX()) { // UN TRAITEMENT PAR LIGNE
+				System.out.println("traitement en X="+ lplein.get(i).getX() );
+				Case[] ligne = this.lcase[lplein.get(i).getX() ];
+				// traitement par ligne mouvement horizontal
+				int taille=0;
+				for( int k=0;k<ligne.length;k++){
+					if (ligne[k] != null)
+						taille++;
+				}
+				
+				// un seul élément
+				if (taille == 1) {
+					if (ligne[TAILLE-1]== null) { // pas déjà a la fin
+						int col=-1;
+						for (int j = 0; j < ligne.length; j++) {
+							if (ligne[j]!=null) {
+								col=j;
+							}	
+						}
+						this.lcase[lplein.get(i).getX()][TAILLE-1]=this.lcase[lplein.get(i).getX()][col];
+						this.lcase[lplein.get(i).getX()][TAILLE-1].setPos(new Position(lplein.get(i).getX(),TAILLE-1));
+						this.lcase[lplein.get(i).getX()][col] = null;
+						
+					}
+				}
+				
+				
+				
+				temp=lplein.get(i).getX(); // var temporaire pour traitement par ligne
+			}
+		}
+		
+	}
 	
 	
 }
